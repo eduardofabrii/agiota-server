daf73a444f827eadde577ee11dcb6500aad537b6
eduardofabri <eduardo.fabri@wises.com.br>
2025-08-19 21:36:46 -0300

    Agiota Bank Start

---
+++ src/main/java/com/agiota/bank/AgiotaBankApplication.java
@@ -0,0 +1,13 @@
+package com.agiota.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiotaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiotaBankApplication.class, args);
+	}
+
+}
