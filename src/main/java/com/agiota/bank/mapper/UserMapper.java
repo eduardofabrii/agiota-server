722a1a1a71a02f9ffe28b598380baa50779acce3
eduardofabrii <eduardo.fabri@wises.com.br>
2025-08-26 20:13:30 -0300

    fix: erros do user mapper e setando usuario admin

--- src/main/java/com/agiota/bank/mapper/UserMapper.java
+++ src/main/java/com/agiota/bank/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.agiota.bank.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 import com.agiota.bank.dto.request.UserRequestDTO;
@@ -12,6 +13,10 @@ import com.agiota.bank.model.user.User;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "lastLogin", ignore = true)
     User toUserPostRequest(UserRequestDTO userRequest);
 
     UserResponseDTO toUserPostResponse(User user);
