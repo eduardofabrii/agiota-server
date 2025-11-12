5f1dc9b15555f3901e27e872f2ef8263c476bdde
eduardofabrii <eduardo.fabri@wises.com.br>
2025-11-11 21:10:15 -0300

    fix: users

--- src/main/java/com/agiota/bank/dto/request/UserRequestDTO.java
+++ src/main/java/com/agiota/bank/dto/request/UserRequestDTO.java
@@ -3,10 +3,10 @@ package com.agiota.bank.dto.request;
 import com.agiota.bank.model.user.UserRole;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record UserRequestDTO(String name,
                              String password,
                              @Email String email,
-                            @NotBlank UserRole role) {
+                             @NotNull UserRole role) {
 }
